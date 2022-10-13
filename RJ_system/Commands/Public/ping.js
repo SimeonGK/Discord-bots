694f897586bedad2ab9fdffaf8211f434126eef8
Simeon <s.g.klumperbeek@gmail.com>
2022-10-13 17:21:21 +0200

    commandHandler

---
+++ RJ_system/Commands/Public/ping.js
@@ -0,0 +1,18 @@
+const {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Will respond with pong."),
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interactin
+   */
+  execute(interactin) {
+    interactin.reply({ content: "Pong! reloaded", ephemeral: true });
+    console.log("Responded with ping reloaded");
+  },
+};
